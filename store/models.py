35e8f65f478f7384e0349425b861859ed6d8d9cb
Mahdi Ranjbar <55695929+MahdiRanjbar79@users.noreply.github.com>
2021-08-04 13:19:25 +0430

    Selecting related Object SRO

--- store/models.py
+++ store/models.py
@@ -11,10 +11,10 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
 
-    class Meta :
+    class Meta:
         ordering = ['title']
 
 
@@ -52,6 +52,12 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
 
 
 class Order(models.Model):
