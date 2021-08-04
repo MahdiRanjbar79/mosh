5e10d21fce4d117278f008ba842804c7ba68c942
Mahdi Ranjbar <55695929+MahdiRanjbar79@users.noreply.github.com>
2021-08-04 12:33:27 +0430

    Customizing the list Page

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-from store.models import Product
-from django.core.exceptions import ObjectDoesNotExist
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(description__isnull=True)
-
-    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Mosh'})
--- store/models.py
+++ store/models.py
@@ -2,25 +2,37 @@ from django.db import models
 
 
 class Promotion(models.Model):
-    description = models.CharField(max_length = 255)
+    description = models.CharField(max_length=255)
     discount = models.FloatField()
 
 
 class Collection(models.Model):
-    title = models.CharField(max_length = 255)
+    title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(
-        'Product', on_delete = models.SET_NULL, null = True, related_name = '+')
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+    def __str__(self):
+        return self.title
+
+    class Meta :
+        ordering = ['title']
 
 
 class Product(models.Model):
-    title = models.CharField(max_length = 255)
+    title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField()
-    unit_price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
-    last_update = models.DateTimeField(auto_now = True)
-    collection = models.ForeignKey(Collection, on_delete = models.PROTECT)
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
 
 
 class Customer(models.Model):
@@ -33,13 +45,13 @@ class Customer(models.Model):
             (MEMBERSHIP_SILVER, 'Silver'),
             (MEMBERSHIP_GOLD, 'Gold'),
     ]
-    first_name = models.CharField(max_length = 255)
-    last_name = models.CharField(max_length = 255)
-    email = models.EmailField(unique = True)
-    phone = models.CharField(max_length = 255)
-    birth_date = models.DateField(null = True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
     membership = models.CharField(
-        max_length = 1, choices = MEMBERSHIP_CHOICES, default = MEMBERSHIP_BRONZE)
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
 
 class Order(models.Model):
@@ -52,31 +64,31 @@ class Order(models.Model):
             (PAYMENT_STATUS_FAILED, 'Failed')
     ]
 
-    placed_at = models.DateTimeField(auto_now_add = True)
+    placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
-        max_length = 1, choices = PAYMENT_STATUS_CHOICES, default = PAYMENT_STATUS_PENDING)
-    customer = models.ForeignKey(Customer, on_delete = models.PROTECT)
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete = models.PROTECT)
-    product = models.ForeignKey(Product, on_delete = models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 class Address(models.Model):
-    street = models.CharField(max_length = 255)
-    city = models.CharField(max_length = 255)
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
     customer = models.ForeignKey(
-        Customer, on_delete = models.CASCADE)
+        Customer, on_delete=models.CASCADE)
 
 
 class Cart(models.Model):
-    created_at = models.DateTimeField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete = models.CASCADE)
-    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
