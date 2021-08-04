1245172816d7933e598929d48b24a3294822777a
Mahdi Ranjbar <55695929+MahdiRanjbar79@users.noreply.github.com>
2021-08-04 13:27:40 +0430

    ordering the base queryset

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.http import HttpRequest
+
 from . import models
 
 
@@ -32,4 +35,15 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'placed_at', 'customer']
 
 
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ["title", 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        return collection.products_count
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
